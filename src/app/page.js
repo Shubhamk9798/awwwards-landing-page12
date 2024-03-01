c38bc7d9a267492f2e85c3246d269bc6ebaeb25c
Shubhamk9798 <134265040+Shubhamk9798@users.noreply.github.com>
2024-03-01 21:05:26 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,10 @@ import styles from './page.module.scss'
 import { useEffect, useState } from 'react'
 import { AnimatePresence } from 'framer-motion';
 import Preloader from '../components/Preloader';
-import Landing from '../components/Landing';
+import Landing from '../components/Landing/Index';
 import Projects from '../components/Projects';
 import Description from '../components/Description';
-import SlidingImages from '../components/SlidingImages';
+import SlidingImages from '../components/SlidingImages/Index';
 import Contact from '../components/Contact';
 
 export default function Home() {
